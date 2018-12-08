1289f7b709fb2e3d731d07e1e289c731bfaec0bd
helaliFerid <helaliferid@gmai.com>
2018-12-08 18:22:17 +0100

    add authgard and fix garded path

--- src/app/order/order-routing.module.ts
+++ src/app/order/order-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { OrderListComponent } from './order-list/order-list.component';
+import { AuthGuard } from '../user/_guard/auth.guard';
 
 const routes: Routes = [
   {
     path: 'order',
-    component: OrderListComponent
+    component: OrderListComponent,
+    canActivate: [AuthGuard]
   }
 ];
 
